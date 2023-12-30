b0e90188726693bac81518930e4798d4b08707fc
SangChu04 <drchasekim@gmail.com>
2023-12-31 01:15:29 +0900

    게시물 컴포넌트 생성

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+import UserInfo from './UserInfo'
+import PostImage from './PostImage'
+import ReactSection from './ReactSection'
+
+const Container = styled.article`
+	display: flex;
+	flex-direction: column;
+	height: fit-content;
+	width: 30rem;
+	border-bottom: 1px solid gray;
+	margin-bottom: 1rem;
+	padding-bottom: 1rem;
+	box-sizing: border-box;
+`
+
+const UserInfoContainer = styled.div`
+	display: flex;
+	width: 100%;
+	flex-direction: row;
+	justify-content: space-between;
+	padding-bottom: 0.75rem;
+	padding-left: 0.25rem;
+	box-sizing: border-box;
+`
+
+const ExtraButton = styled.button`
+	background-color: transparent;
+	border: none;
+	box-sizing: border-box;
+	padding: 0;
+	display: flex;
+	align-items: center;
+	& img {
+		width: 2em;
+		height: 1.5em;
+	}
+`
+
+export default function Post() {
+	return (
+		<Container>
+			<UserInfoContainer>
+				<UserInfo />
+				<ExtraButton>
+					<img src="/ellipsis.svg" alt="ellipsis" />
+				</ExtraButton>
+			</UserInfoContainer>
+			<PostImage />
+			<ReactSection />
+		</Container>
+	)
+}
