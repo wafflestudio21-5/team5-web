d51a7fb8a43f97c43368896c1c56a131616ff018
SangChu04 <drchasekim@gmail.com>
2023-12-31 02:07:03 +0900

    Home 페이지 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
+import styled from 'styled-components'
+import PostList from '../components/Post/PostList'
+
+const HomeLayout = styled.main`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	overflow-y: scroll;
+	margin-top: 1rem;
+	justify-content: center;
+	& .story-post {
+		display: flex;
+		flex-direction: column;
+	}
+`
+
 export default function Home() {
 	return (
-		<>
-			<div>홈</div>
-		</>
+		<HomeLayout>
+			<div className="story-post">
+				<PostList />
+			</div>
+		</HomeLayout>
 	)
 }
