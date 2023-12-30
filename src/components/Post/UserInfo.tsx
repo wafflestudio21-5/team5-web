f820bab02eb873e560170b2676784db3ba8f2df1
SangChu04 <drchasekim@gmail.com>
2023-12-30 22:24:20 +0900

    게시물 올린 사용자 정보 표시 컴포먼트 생성

---
+++ src/components/Post/UserInfo.tsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+	display: flex;
+	gap: 1rem;
+`
+
+const StyledLink = styled.a`
+	text-decoration: none;
+	color: black;
+`
+
+const ImageBox = styled.div`
+	width: 2rem;
+	height: 2rem;
+	border-radius: 70%;
+	overflow: hidden;
+	display: inline;
+`
+
+const ProfileImage = styled.img`
+	width: 100%;
+	height: 100%;
+	object-fit: cover;
+`
+
+const NameBox = styled.div`
+	display: flex;
+	align-items: center;
+`
+
+export default function UserInfo() {
+	return (
+		<StyledLink>
+			<Container>
+				<ImageBox>
+					<ProfileImage src="" alt="profile image" />
+				</ImageBox>
+				<NameBox>
+					<span>
+						<b>sangchu</b>
+					</span>
+				</NameBox>
+			</Container>
+		</StyledLink>
+	)
+}
