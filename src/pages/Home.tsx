a61eabf2580f03219fc957e9bf93500f580d9182
SangChu04 <drchasekim@gmail.com>
2024-01-07 19:54:00 +0900

    홈 화면에 스토리 바 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,18 @@
 import styled from 'styled-components'
 import PostList from '../components/Post/PostList'
 import Header from '../components/Header'
+import StoryBar from '../components/Story/StoryBar'
 
 const HomeLayout = styled.main`
 	width: 100%;
 	display: flex;
 	flex-direction: row;
-	overflow-y: scroll;
+	overflow-y: hidden;
 	margin-top: 1rem;
 	justify-content: center;
+	padding: 0;
 	& .story-post {
+		width: 30rem;
 		display: flex;
 		flex-direction: column;
 	}
@@ -21,6 +24,7 @@ export default function Home() {
 			<Header />
 			<HomeLayout>
 				<div className="story-post">
+					<StoryBar />
 					<PostList />
 				</div>
 			</HomeLayout>
