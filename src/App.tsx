dc1c02acd96ccc790e04ffed8efeb9d9fb98730e
Jaewoong Choi <jwchoi.tech@gmail.com>
2024-01-11 23:29:58 +0900

    feat: CICD 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import { useUserContext } from './contexts/UserContext.tsx'
+// import { useUserContext } from './contexts/UserContext.tsx'
 import Explore from './pages/Explore.tsx'
 import Home from './pages/Home.tsx'
 import Profile from './pages/Profile/Profile.tsx'
 import Reels from './pages/Reels.tsx'
-import Login from './pages/Login/Login.tsx'
-import PasswordRecovery from './pages/Login/passwordRecovery/PasswordRecovery.tsx'
-import SignUp from './pages/Login/signUp/SignUp.tsx'
-import Certification from './pages/Login/passwordRecovery/Certification.tsx'
-import NewPassword from './pages/Login/passwordRecovery/NewPassword.tsx'
+// import Login from './pages/Login/Login.tsx'
+// import PasswordRecovery from './pages/Login/passwordRecovery/PasswordRecovery.tsx'
+// import SignUp from './pages/Login/signUp/SignUp.tsx'
+// import Certification from './pages/Login/passwordRecovery/Certification.tsx'
+// import NewPassword from './pages/Login/passwordRecovery/NewPassword.tsx'
 // import MakePassword from './pages/Login/signIn/MakePassword.tsx'
 // import AskToSave from './pages/Login/signIn/AskToSave.tsx'
 // import AskBirthday from './pages/Login/signIn/AskBirthday.tsx'
@@ -17,7 +17,7 @@ import NewPassword from './pages/Login/passwordRecovery/NewPassword.tsx'
 // import CertificationSignIn from './pages/Login/signIn/CertificationSignIn.tsx'
 // import Agree from './pages/Login/signIn/Agree.tsx'
 // import AddPhoto from './pages/Login/signIn/AddPhoto.tsx'
-import CertificationSignUp from './pages/Login/signUp/CertificationSignUp.tsx'
+// import CertificationSignUp from './pages/Login/signUp/CertificationSignUp.tsx'
 import EditProfile from './pages/Profile/EditProfile.tsx'
 import Follow from './pages/Profile/Follow.tsx'
 import GlobalStyles from './styles/GlobalStyles.tsx'
@@ -61,64 +61,64 @@ const router = createBrowserRouter([
 	},
 ])
 
-const loginRouter = createBrowserRouter([
-	{
-		path: '',
-		element: <Login />,
-	},
-	{
-		path: 'passwordRecovery/',
-		element: <PasswordRecovery />,
-	},
-	{
-		path: 'passwordRecovery/certification/',
-		element: <Certification />,
-	},
-	{
-		path: 'passwordRecovery/newPassword/',
-		element: <NewPassword />,
-	},
-	{
-		path: 'signUp/',
-		element: <SignUp />,
-	},
-	// {
-	// 	path: 'signUp/password/',
-	// 	element: <MakePassword />,
-	// },
-	// {
-	// 	path: 'signUp/save/',
-	// 	element: <AskToSave />,
-	// },
-	// {
-	// 	path: 'signUp/birthday/',
-	// 	element: <AskBirthday />,
-	// },
-	// {
-	// 	path: 'signUp/username/',
-	// 	element: <MakeUsername />,
-	// },
-	// {
-	// 	path: 'signUp/email/',
-	// 	element: <AskEmail />,
-	// },
-	{
-		path: 'signUp/certification/',
-		element: <CertificationSignUp />,
-	},
-	// {
-	// 	path: 'signUp/agreeToTerm/',
-	// 	element: <Agree />,
-	// },
-	// {
-	// 	path: 'signUp/photo/',
-	// 	element: <AddPhoto />,
-	// },
-	{
-		path: '*',
-		element: <Navigate to="/" />,
-	},
-])
+// const loginRouter = createBrowserRouter([
+// 	{
+// 		path: '',
+// 		element: <Login />,
+// 	},
+// 	{
+// 		path: 'passwordRecovery/',
+// 		element: <PasswordRecovery />,
+// 	},
+// 	{
+// 		path: 'passwordRecovery/certification/',
+// 		element: <Certification />,
+// 	},
+// 	{
+// 		path: 'passwordRecovery/newPassword/',
+// 		element: <NewPassword />,
+// 	},
+// 	{
+// 		path: 'signUp/',
+// 		element: <SignUp />,
+// 	},
+// 	// {
+// 	// 	path: 'signUp/password/',
+// 	// 	element: <MakePassword />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/save/',
+// 	// 	element: <AskToSave />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/birthday/',
+// 	// 	element: <AskBirthday />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/username/',
+// 	// 	element: <MakeUsername />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/email/',
+// 	// 	element: <AskEmail />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/certification/',
+// 	// 	element: <CertificationSignUp />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/agreeToTerm/',
+// 	// 	element: <Agree />,
+// 	// },
+// 	// {
+// 	// 	path: 'signUp/photo/',
+// 	// 	element: <AddPhoto />,
+// 	// },
+// 	{
+// 		path: '*',
+// 		element: <Navigate to="/" />,
+// 	},
+// ])
 
 function App() {
 	return (
--- src/components/Profile/MenuModal.tsx
+++ src/components/Profile/MenuModal.tsx
@@ -1,14 +1,14 @@
-import styled from 'styled-components'
-import Modal from '../../shared/Modal.tsx'
-
-export default function MenuModal({
-	onCloseMenuModal,
-}: {
-	onCloseMenuModal: () => void
-}) {
-	return (
-		<>
-			<Modal></Modal>
-		</>
-	)
-}
+// import styled from 'styled-components'
+// import Modal from '../../shared/Modal.tsx'
+//
+// export default function MenuModal({
+// 	onCloseMenuModal,
+// }: {
+// 	onCloseMenuModal: () => void
+// }) {
+// 	return (
+// 		<>
+// 			<Modal></Modal>
+// 		</>
+// 	)
+// }
