8b2c49492a2b22d19621c031c5530ed349a45e68
SangChu04 <drchasekim@gmail.com>
2024-01-12 14:19:04 +0900

    feat: PostImage에 imageUrl Props 추가

--- src/components/Post/PostImage.tsx
+++ src/components/Post/PostImage.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import type { Post } from '../../contexts/PostContext'
 
 const Container = styled.div`
 	position: relative;
@@ -14,61 +15,65 @@ const Image = styled.img`
 	position: relative;
 `
 
-const MoveButton = styled.button`
-	position: absolute;
-	top: calc(50% - 0.625rem);
-	width: 1.25rem;
-	height: 1.25rem;
-	padding: 0;
-	border-radius: 70%;
-	border: 0;
-	background-color: rgba(245, 245, 245, 0.7);
-	color: black;
-	&.left {
-		left: 1rem;
-	}
-	&.right {
-		right: 1rem;
-	}
-`
+// 이미지가 여러 개 있을 때 보이는 좌우 이동 버튼과 인덱스 표시 수평점
+// 여러 이미지를 받을 지 결정이 안되어서 주석처리 함.
 
-const IndexBar = styled.div`
-	position: absolute;
-	width: 100%;
-	height: fit-content;
-	bottom: 1rem;
-	left: 0;
-	padding: 0;
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-	& .dot {
-		width: 0.375rem;
-		height: 0.375rem;
-		border-radius: 50%;
-		background-color: rgba(255, 255, 255, 0.4);
-		margin-right: 0.25rem;
-	}
-	& .select {
-		background-color: rgb(255, 255, 255);
-		margin-right: 0;
-	}
-`
+// const MoveButton = styled.button`
+// 	position: absolute;
+// 	top: calc(50% - 0.625rem);
+// 	width: 1.25rem;
+// 	height: 1.25rem;
+// 	padding: 0;
+// 	border-radius: 70%;
+// 	border: 0;
+// 	background-color: rgba(245, 245, 245, 0.7);
+// 	color: black;
+// 	&.left {
+// 		left: 1rem;
+// 	}
+// 	&.right {
+// 		right: 1rem;
+// 	}
+// `
 
-export default function PostImage() {
+// const IndexBar = styled.div`
+// 	position: absolute;
+// 	width: 100%;
+// 	height: fit-content;
+// 	bottom: 1rem;
+// 	left: 0;
+// 	padding: 0;
+// 	display: flex;
+// 	flex-direction: row;
+// 	justify-content: center;
+// 	& .dot {
+// 		width: 0.375rem;
+// 		height: 0.375rem;
+// 		border-radius: 50%;
+// 		background-color: rgba(255, 255, 255, 0.4);
+// 		margin-right: 0.25rem;
+// 	}
+// 	& .select {
+// 		background-color: rgb(255, 255, 255);
+// 		margin-right: 0;
+// 	}
+// `
+
+export default function PostImage({
+	imageUrl,
+}: {
+	imageUrl: Post['imageUrl']
+}) {
 	return (
 		<Container>
-			<Image
-				src="https://dimg.donga.com/wps/NEWS/IMAGE/2023/07/05/120093215.1.edit.jpg"
-				alt="post image"
-			/>
-			<MoveButton className="left">&lt;</MoveButton>
+			<Image src={imageUrl} alt="post image" />
+			{/* <MoveButton className="left">&lt;</MoveButton>
 			<MoveButton className="right">&gt;</MoveButton>
 			<IndexBar>
 				<div className="dot"></div>
 				<div className="dot"></div>
 				<div className="dot select"></div>
-			</IndexBar>
+			</IndexBar> */}
 		</Container>
 	)
 }
