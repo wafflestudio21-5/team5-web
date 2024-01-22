faca17037508fde48a113114d733d356a40ac2e8
SangChu04 <drchasekim@gmail.com>
2024-01-22 13:51:31 +0900

    feat: 댓글 리스트 컴포넌트 생성

---
+++ src/components/Comment/CommentList.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+
+import { CommentType } from '../../types';
+
+import Comment from './Comment';
+
+export default function CommentList() {
+	const comments: CommentType[] = [
+		{
+			commentId: 1,
+			userId: 1,
+			username: 'sangchu',
+			content: '안녕하세요',
+			createdAt: '2023-01-01T12:30:00.000Z',
+		},
+		{
+			commentId: 2,
+			userId: 2,
+			username: 'gamja',
+			content: '멋있어요!',
+			createdAt: '2023-01-01T12:31:00.000Z',
+		},
+		{
+			commentId: 3,
+			userId: 3,
+			username: 'iamjam',
+			content: '미ㅏ어리어ㅣㄹ마어ㅣㅏㅓㅁㄹ아ㅣㅁㄹ',
+			createdAt: '2023-01-01T12:35:00.000Z',
+		},
+	];
+
+	useEffect(() => {});
+
+	return (
+		<ul>
+			{comments.map((comment) => (
+				<Comment comment={comment} />
+			))}
+		</ul>
+	);
+}
