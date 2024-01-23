b70de8b837c873b96796c38f01a1013dfd71c2f4
SangChu04 <drchasekim@gmail.com>
2024-01-24 01:51:21 +0900

    feat: 탐색 탭에 PostList 추가 (예시)

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -1,7 +1,11 @@
+import { feed } from '../../components/Feed';
+import PostList from '../../components/Post/PostList';
+
 export default function Explore() {
 	return (
 		<>
 			<div>포스트 필터링 탭 추가 예정</div>
+			<PostList posts={feed.posts} />
 		</>
 	);
 }
