1f219bf06cbd20dd59e5368f0ea92c38921f00a2
onezero01010 <rachelsong10@snu.ac.kr>
2024-01-26 02:37:12 +0900

    fix: addtext file 수정 테스트

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,43 +1,33 @@
 module.exports = {
-	root: true,
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-		// Add the recommended import rules
-		'plugin:import/errors',
-		'plugin:import/warnings',
-		'plugin:import/typescript',
-		'plugin:prettier/recommended',
-		'prettier',
-	],
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parser: '@typescript-eslint/parser',
-	plugins: ['react-refresh', 'import', 'prettier'], // Add 'import' to the plugins array
-	rules: {
-		'react-refresh/only-export-components': [
-			'warn',
-			{ allowConstantExport: true },
-		],
-		semi: ['error', 'always'],
-		'prettier/prettier': ['error'],
-		// Add specific import rules
-		'import/order': [
-			'error',
-			{
-				groups: [
-					'builtin',
-					'external',
-					'internal',
-					'parent',
-					'sibling',
-					'index',
-				],
-				'newlines-between': 'always',
-				alphabetize: { order: 'asc', caseInsensitive: true },
-			},
-		],
-		'import/no-duplicates': 'error',
-	},
-};
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    // Add the recommended import rules
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'import', 'prettier'], // Add 'import' to the plugins array
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'semi': ['error', 'always'],
+    'prettier/prettier': ['error'],
+    // Add specific import rules
+    'import/order': ['error', {
+      'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+      'newlines-between': 'always',
+      'alphabetize': { 'order': 'asc', 'caseInsensitive': true }
+    }],
+    'import/no-duplicates': 'error',
+  },
+}
