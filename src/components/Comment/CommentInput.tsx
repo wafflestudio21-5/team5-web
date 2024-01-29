09b0eb409f824ca7ebd774a3b4e6e35941cb3964
SangChu04 <drchasekim@gmail.com>
2024-01-29 12:30:47 +0900

    feat: 답글 정보창 생성

--- src/components/Comment/CommentInput.tsx
+++ src/components/Comment/CommentInput.tsx
@@ -9,16 +9,37 @@ import { CommentType, MiniProfileType, PostType } from '../../types';
 
 const CommentInputContainer = styled.div`
 	display: flex;
-	flex-direction: row;
+	flex-direction: column;
 	align-items: center;
 	position: sticky;
 	bottom: 0;
+	width: 100%;
+	margin-top: 1rem;
+	& > .reply-info {
+		justify-content: space-between;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: row;
+		width: 100%;
+		background-color: ${getColor('grey')};
+		color: ${getColor('darkGrey')};
+		padding: 0.5rem;
+	}
+	& > .reply-info > button {
+		background-color: transparent;
+		border: none;
+		color: ${getColor('darkGrey')};
+	}
+`;
+
+const CommentInputFeildWrapper = styled.div`
+	display: flex;
+	flex-direction: row;
 	border-top: 1px solid ${getColor('grey')};
 	width: 100%;
 	gap: 1rem;
-	margin-top: 1rem;
-	padding-top: 1rem;
-	padding-bottom: 1rem;
+	box-sizing: border-box;
+	padding: 0.5rem 1rem 0.5rem 1rem;
 	& > .profile-image-wrapper {
 		width: 2rem;
 		height: 2rem;
@@ -67,13 +88,14 @@ const CommentInputContainer = styled.div`
 	& > .input-btn-wrapper > .btn-wrapper {
 		display: flex;
 		height: 100%;
-		align-content: baseline;
+		align-items: flex-end;
 		cursor: pointer;
 	}
 	& > .input-btn-wrapper > .btn-wrapper > button {
 		border: none;
 		background-color: transparent;
 		width: 2rem;
+		height: 2rem;
 		padding: 0;
 		color: ${getColor('blue')};
 		font-weight: 600;
@@ -112,55 +134,65 @@ export default function CommentInput({
 
 	return (
 		<CommentInputContainer>
-			<div className="profile-image-wrapper">
-				<img
-					src={
-						user.profileImageUrl !== '' ? user.profileImageUrl : DefaultProfile
-					}
-				/>
-			</div>
-			<div className="input-btn-wrapper">
-				<div className="input-wrapper">
-					<textarea
-						rows={1}
-						value={content}
-						onChange={(e) => {
-							setContent(e.target.value);
-							e.currentTarget.style.height = 'auto';
-							e.currentTarget.style.height =
-								e.currentTarget.scrollHeight + 'px';
-						}}
-						placeholder={user.username + '(으)로 댓글 달기...'}
-					></textarea>
+			{commentType === 'reply' && comment !== null && (
+				<div className="reply-info">
+					<span>{comment.user.username}에게 남긴 답글</span>
+					<button>X</button>
 				</div>
-				{content !== '' && (
-					<div className="btn-wrapper">
-						<button
-							onClick={async () => {
-								if (content !== '') {
-									if (commentType === 'reply' && comment) {
-										const result = await postReply(
-											comment.id,
-											content,
-											accessToken
-										);
-										if (result?.status) setContent('');
-									} else if (commentType === 'comment' && post.id) {
-										const result = await postComment(
-											post.id,
-											content,
-											accessToken
-										);
-										if (result?.status) setContent('');
-									}
-								}
+			)}
+			<CommentInputFeildWrapper>
+				<div className="profile-image-wrapper">
+					<img
+						src={
+							user.profileImageUrl !== ''
+								? user.profileImageUrl
+								: DefaultProfile
+						}
+					/>
+				</div>
+				<div className="input-btn-wrapper">
+					<div className="input-wrapper">
+						<textarea
+							rows={1}
+							value={content}
+							onChange={(e) => {
+								setContent(e.target.value);
+								e.currentTarget.style.height = 'auto';
+								e.currentTarget.style.height =
+									e.currentTarget.scrollHeight + 'px';
 							}}
-						>
-							게시
-						</button>
+							placeholder={user.username + '(으)로 댓글 달기...'}
+						></textarea>
 					</div>
-				)}
-			</div>
+					{content !== '' && (
+						<div className="btn-wrapper">
+							<button
+								onClick={async () => {
+									if (content !== '') {
+										if (commentType === 'reply' && comment) {
+											const result = await postReply(
+												comment.id,
+												content,
+												accessToken
+											);
+											if (result?.status) setContent('');
+										} else if (commentType === 'comment' && post.id) {
+											const result = await postComment(
+												post.id,
+												content,
+												accessToken
+											);
+											if (result?.status) setContent('');
+										}
+									}
+								}}
+							>
+								게시
+							</button>
+						</div>
+					)}
+				</div>
+			</CommentInputFeildWrapper>
 		</CommentInputContainer>
 	);
 }
