2db52f0e99e0121cecc5504c4a44096a59e1691b
SangChu04 <drchasekim@gmail.com>
2024-01-30 16:22:52 +0900

    feat: 게시물 여러 이미지 처리

--- src/components/Post/PostImage.tsx
+++ src/components/Post/PostImage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 import { MediaType } from '../../types';
@@ -15,61 +16,76 @@ const Image = styled.img`
 	position: relative;
 `;
 
-// 이미지가 여러 개 있을 때 보이는 좌우 이동 버튼과 인덱스 표시 수평점
-// 여러 이미지를 받을 지 결정이 안되어서 주석처리 함.
+const MoveButton = styled.button`
+	position: absolute;
+	top: calc(50% - 0.625rem);
+	width: 1.25rem;
+	height: 1.25rem;
+	padding: 0;
+	border-radius: 70%;
+	border: 0;
+	background-color: rgba(245, 245, 245, 0.7);
+	color: black;
+	&.left {
+		left: 1rem;
+	}
+	&.right {
+		right: 1rem;
+	}
+`;
 
-// const MoveButton = styled.button`
-// 	position: absolute;
-// 	top: calc(50% - 0.625rem);
-// 	width: 1.25rem;
-// 	height: 1.25rem;
-// 	padding: 0;
-// 	border-radius: 70%;
-// 	border: 0;
-// 	background-color: rgba(245, 245, 245, 0.7);
-// 	color: black;
-// 	&.left {
-// 		left: 1rem;
-// 	}
-// 	&.right {
-// 		right: 1rem;
-// 	}
-// `
-
-// const IndexBar = styled.div`
-// 	position: absolute;
-// 	width: 100%;
-// 	height: fit-content;
-// 	bottom: 1rem;
-// 	left: 0;
-// 	padding: 0;
-// 	display: flex;
-// 	flex-direction: row;
-// 	justify-content: center;
-// 	& .dot {
-// 		width: 0.375rem;
-// 		height: 0.375rem;
-// 		border-radius: 50%;
-// 		background-color: rgba(255, 255, 255, 0.4);
-// 		margin-right: 0.25rem;
-// 	}
-// 	& .select {
-// 		background-color: rgb(255, 255, 255);
-// 		margin-right: 0;
-// 	}
-// `
+const IndexBar = styled.div`
+	position: absolute;
+	width: 100%;
+	height: fit-content;
+	bottom: 1rem;
+	left: 0;
+	padding: 0;
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	& .dot {
+		width: 0.375rem;
+		height: 0.375rem;
+		border-radius: 50%;
+		background-color: rgba(255, 255, 255, 0.4);
+		margin-right: 0.25rem;
+	}
+	& .select {
+		background-color: rgb(255, 255, 255);
+	}
+`;
 
 export default function PostImage({ media }: { media: MediaType[] }) {
+	const [order, setOrder] = useState(0);
 	return (
 		<Container>
-			<Image src={media[0].url} alt="post image" />
-			{/* <MoveButton className="left">&lt;</MoveButton>
-			<MoveButton className="right">&gt;</MoveButton>
-			<IndexBar>
-				<div className="dot"></div>
-				<div className="dot"></div>
-				<div className="dot select"></div>
-			</IndexBar> */}
+			<Image src={media[order].url} alt="post image" />
+			{media.length > 1 && (
+				<>
+					{order > 0 && (
+						<MoveButton className="left" onClick={() => setOrder(order - 1)}>
+							&lt;
+						</MoveButton>
+					)}
+					{order + 1 < media.length && (
+						<MoveButton className="right" onClick={() => setOrder(order + 1)}>
+							&gt;
+						</MoveButton>
+					)}
+					<IndexBar>
+						{media.map((m, index) => {
+							console.log(m.order, index);
+							return (
+								<div
+									className={`dot ${m.order === order + 1 ? 'select' : ''}`}
+									key={m.order}
+								></div>
+							);
+						})}
+					</IndexBar>
+				</>
+			)}
 		</Container>
 	);
 }
