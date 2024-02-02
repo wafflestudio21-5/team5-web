b6ae40fe3c5505bdca3bb183fae36d59b4a5acfb
onezero01010 <rachelsong10@snu.ac.kr>
2024-02-03 03:02:37 +0900

    App.tsx에 tokenrefresher 디버깅

--- src/apis/TokenRefresher.tsx
+++ src/apis/TokenRefresher.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 import { resetAccessToken } from './login';
-import { useUserContext } from '../contexts/UserContext';
 
 export default function TokenRefresher() {
 	console.log('in');
