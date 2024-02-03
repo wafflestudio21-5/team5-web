9721a776e3e6bc5f48c5664ed9a43073064a6455
onezero01010 <rachelsong10@snu.ac.kr>
2024-02-03 11:35:37 +0900

    새 브랜치에 feat/login2 기능 재구현

--- src/App.tsx
+++ src/App.tsx
@@ -44,6 +44,7 @@ import UserFeed from './pages/Profile/UserFeed.tsx';
 import Search from './pages/Search.tsx';
 import GlobalStyles from './styles/GlobalStyles.tsx';
 import Theme from './styles/Theme.tsx';
+import TokenRefresher from './apis/TokenRefresher.tsx';
 
 const router = createBrowserRouter([
 	{
@@ -211,6 +212,7 @@ export default function App() {
 
 	return (
 		<ThemeProvider theme={Theme}>
+			<TokenRefresher />
 			<GlobalStyles />
 			<RouterProvider router={isLoggedIn ? router : loginRouter} />
 		</ThemeProvider>
---
+++ src/apis/TokenRefresher.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { resetAccessToken } from './login';
+import { useUserContext } from '../contexts/UserContext';
+
+export default function TokenRefresher() {
+	console.log('in');
+	const { setAccessToken } = useUserContext();
+
+	useEffect(() => {
+		const interceptor = axios.interceptors.response.use(
+			function (response) {
+				return response;
+			},
+			async function (error) {
+				const originConfig = error.config;
+				if (error.response.status === 401) {
+					const responseData = await resetAccessToken();
+					const newAccessToken = responseData.accessToken;
+					setAccessToken(newAccessToken);
+					originConfig.headers['Authorization'] =
+						'Bearer ' + responseData.accessToken;
+					return axios(error.config);
+				}
+			}
+		);
+		return () => {
+			axios.interceptors.response.eject(interceptor);
+		};
+	}, []);
+	return <></>;
+}
